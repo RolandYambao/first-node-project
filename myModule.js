c05c2a9e4d99aec9cd6306fee9f258952cde9126
RolandYambao <rolandyambao@gmail.com>
2021-11-16 13:24:46 -0800

    Updated

---
+++ myModule.js
@@ -0,0 +1,12 @@
+function helloClass() {
+    return "Good morning. Please put sonic emoji in Slack."
+}
+
+// run this function
+console.log(helloClass());
+
+// *** how would we run this function ****
+// consol elog it
+// node index.js
+// npm run start
+// link inside of index
