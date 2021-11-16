ace0035e7c6803506e8fe2dd692782315cab5c60
RolandYambao <rolandyambao@gmail.com>
2021-11-16 14:22:07 -0800

    Updated

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const numbers = [1, 2, 4, 6, 22, 34];
 const person = {
     name: "John Doe",
     age: 39,
-    foods: ["burgers", "fries"],
+    foods: ['burgers', 'fries'],
     car: {
-        name: "Tesla",
-        model: "Model S",
-        year: 2020,
+        name: 'Tesla',
+        model: 'Model S',
+        year: 2020
     }
-};
+}
 console.log(numbers);
 console.log(person.foods);
 
-const somethingElse = require("./myModule");
-console.log(somethingElse);
 
-somethingElse.helloClass(); // {string}
-console.log(somethingElse.helloClass()); // => "Good morning. Please put sonic emoji in Slack"
+const somethingElse = require('./utils/myModule');
+
+console.log(somethingElse); // object, 
+
+somethingElse.hello(); // {string}
+console.log(somethingElse.hello()); // => "Good morning. Please put sonic emoji in Slack."
