e46d0052f949bc93dc030d5eb0efcb76c4657746
RolandYambao <rolandyambao@gmail.com>
2021-11-16 11:45:15 -0800

    Updated

---
+++ index.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 4, 6, 22, 34];
+const person = {
+    name: "John Doe",
+    age: 39,
+    foods: ["burgers", "fries"],
+    car: {
+        name: "Tesla",
+        model: "Model S",
+        year: 2020,
+    }
+};
+console.log(numbers);
+console.log(person.foods);
